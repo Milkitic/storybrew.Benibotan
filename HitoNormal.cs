0bb4f76188849a9796b446f4a89239d4b3bfc53f
Milkitic Yang <milkitic@126.com>
2018-03-05 21:40:03 +0800

    Finished HitoNormal

--- HitoNormal.cs
+++ HitoNormal.cs
@@ -20,6 +20,7 @@ namespace StorybrewScripts
         {
             StoryboardLayer lay_fore = GetLayer("Fore");
 
+            // Fox's Shadow
             int shakeDelay = 120;
             var shadow = lay_fore.CreateSprite(@"SB\2dx_69.png", OsbOrigin.BottomCentre, new Vector2(320, 300 + 398 / 2));
             shadow.Fade(startTime + (17155 - 17031) - 100, startTime + (17528 - 17031), 0, 0.6);
@@ -31,6 +32,7 @@ namespace StorybrewScripts
 
             shadow.Fade(startTime + (27639 - 17031), 0.6);
 
+            // Fox's body
             var body = lay_fore.CreateSprite(@"SB\2dx_114.png", OsbOrigin.BottomCentre, new Vector2(320, 285 + 398 / 2));
             body.Fade(startTime, startTime + (17280 - 17031), 0, 1);
             body.MoveX(startTime, startTime + (17280 - 17031), 280, 223);
@@ -41,6 +43,7 @@ namespace StorybrewScripts
 
             body.Fade(startTime + (27639 - 17031), 1);
 
+            // Arrow
             var arrow = lay_fore.CreateSprite(@"SB\2dx_31.png");
             double arrow_deg = 25, arrow_x = 300, arrow_y = 125, length = 600, length2 = 5;
             double arrow_r = arrow_deg / 180 * Math.PI;
@@ -61,6 +64,9 @@ namespace StorybrewScripts
 
             int animationInterval = 70;
             int closedDelay = 100;
+
+            // Code here need to be optimized
+            // Like eye.Blink(timing1, timing2, ...);
             var eye = lay_fore.CreateSprite(@"SB\2dx_70.png", OsbOrigin.Centre, new Vector2(320, 221));
             eye.Fade(startTime, startTime + (17280 - 17031), 0, 1);
             eye.MoveX(startTime, startTime + (17280 - 17031), 289, 232);
@@ -110,34 +116,36 @@ namespace StorybrewScripts
             mouth.MoveX(startTime + (24987 - 17031) + shakeDelay, startTime + (24987 - 17031) + shakeDelay * 2, 220, 228);
             mouth.Rotate(startTime + (24987 - 17031), startTime + (24987 - 17031) + shakeDelay, 0, -0.04);
             mouth.Rotate(startTime + (24987 - 17031) + shakeDelay, startTime + (24987 - 17031) + shakeDelay * 2, -0.04, 0);
-
             // Fade out
             mouth.Fade(startTime + (25153 - 17031), 1);
 
-            int[] say_time = { 25070, 1 };
+            int[] blink_time = { 25070, 1 };
             int ani_interval = 60;
-            for (int i = 0; i < say_time.Length - 1; i += 2)
+            for (int i = 0; i < blink_time.Length - 1; i += 2)
             {
-                for (int j = 0; j < say_time[i + 1]; j++)
+                for (int j = 0; j < blink_time[i + 1]; j++)
                 {
-                    eye.Fade(startTime + (say_time[i] - 17031) + j * ani_interval * 4, 0);
-                    eye2.Fade(startTime + (say_time[i] - 17031) + j * ani_interval * 4, 1);
-                    eye2.Fade(startTime + (say_time[i] - 17031) + ani_interval + j * ani_interval * 4, 0);
-                    eye3.Fade(startTime + (say_time[i] - 17031) + ani_interval + j * ani_interval * 4, 1);
-                    eye3.Fade(startTime + (say_time[i] - 17031) + ani_interval * 3 + j * ani_interval * 4, 0);
-                    eye2.Fade(startTime + (say_time[i] - 17031) + ani_interval * 3 + j * ani_interval * 4, 1);
-                    eye2.Fade(startTime + (say_time[i] - 17031) + ani_interval * 4 + j * ani_interval * 4, 0);
-                    eye.Fade(startTime + (say_time[i] - 17031) + ani_interval * 4 + j * ani_interval * 4, 1);
+                    eye.Fade(startTime + (blink_time[i] - 17031) + j * ani_interval * 4, 0);
+                    eye2.Fade(startTime + (blink_time[i] - 17031) + j * ani_interval * 4, 1);
+                    eye2.Fade(startTime + (blink_time[i] - 17031) + ani_interval + j * ani_interval * 4, 0);
+                    eye3.Fade(startTime + (blink_time[i] - 17031) + ani_interval + j * ani_interval * 4, 1);
+                    eye3.Fade(startTime + (blink_time[i] - 17031) + ani_interval * 3 + j * ani_interval * 4, 0);
+                    eye2.Fade(startTime + (blink_time[i] - 17031) + ani_interval * 3 + j * ani_interval * 4, 1);
+                    eye2.Fade(startTime + (blink_time[i] - 17031) + ani_interval * 4 + j * ani_interval * 4, 0);
+                    eye.Fade(startTime + (blink_time[i] - 17031) + ani_interval * 4 + j * ani_interval * 4, 1);
                 }
             }
+            // Fox's big mouth
             var mouth2 = lay_fore.CreateSprite(@"SB\2dx_39.png", OsbOrigin.Centre, new Vector2(228, 264));
             mouth2.Fade(startTime + (25236 - 17031), startTime + (27639 - 17031), 1, 1);
             mouth2.ScaleVec(startTime + (25236 - 17031), startTime + (25236 - 17031) + 150, 0.9, 0.5, 0.9, 0.9);
 
-            var red = lay_fore.CreateSprite(@"SB\2dx_88.png", OsbOrigin.Centre, new Vector2(219, 240));
-            red.Fade(startTime + (25236 - 17031) - 250, startTime + (25236 - 17031) + 250, 0, 1);
-            red.Fade(startTime + (27639 - 17031), 1);
+            // Blusher on face
+            var blusher = lay_fore.CreateSprite(@"SB\2dx_88.png", OsbOrigin.Centre, new Vector2(219, 240));
+            blusher.Fade(startTime + (25236 - 17031) - 250, startTime + (25236 - 17031) + 250, 0, 1);
+            blusher.Fade(startTime + (27639 - 17031), 1);
 
+            // Hearts in eyes
             var heart = lay_fore.CreateSprite(@"SB\2dx_37.png", OsbOrigin.Centre, new Vector2(203, 218));
             heart.Fade(startTime + (25236 - 17031) + 250, startTime + (25236 - 17031) + 250 + 600, 0, 1);
             heart.Scale(startTime + (25236 - 17031) + 250, startTime + (25236 - 17031) + 250 + 600, 0, 1);
@@ -147,6 +155,20 @@ namespace StorybrewScripts
             heart2.Scale(startTime + (25236 - 17031) + 250, startTime + (25236 - 17031) + 250 + 600, 0, 0.95);
             heart2.Fade(startTime + (27639 - 17031), 1);
             heart2.Rotate(startTime + (25236 - 17031) + 250, 0.1);
+
+            // Big heart
+            var heart3 = lay_fore.CreateSprite(@"SB\2dx_89.png");
+            heart3.Move(startTime + (25401 - 17031), startTime + (25981 - 17031), 380, 230, 410, 220);
+            heart3.Rotate(startTime + (25401 - 17031), startTime + (25981 - 17031), 0.33, 0.5);
+            heart3.Fade(startTime + (25401 - 17031), startTime + (25981 - 17031), 1, 0);
+
+            // Leaves
+            int[] timings = { 27142, 27307 };
+            for (int i = 0; i < timings.Length; i++)
+            {
+                var leaf = lay_fore.CreateSprite(@"SB\2dx_27.png");
+                leaf.Move(startTime + (timings[i] - 17031), startTime + (27639 - 17031), 400 + i * 110, 255, 400 + i * 110, 255);
+            }
         }
     }
 }
